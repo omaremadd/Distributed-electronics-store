5eb0efc7123f34f1ce365e1b897ec0dbd8b3fc68
Mohanad Ahmed <mohanadbador.ma@gmail.com>
2024-05-02 17:20:44 +0300

    removed signup

--- Electronics_App/urls.py
+++ Electronics_App/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('orders/<pk>/', views.OrderDetail.as_view(), name='order_detail'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('signup/', views.signup, name='signup'),
 ]
