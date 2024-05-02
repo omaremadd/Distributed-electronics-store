f1b01d0a026e48806be0d899a0312956d6ec1c84
Omar Emad <167013174+omaremadd@users.noreply.github.com>
2024-05-03 01:54:01 +0300

    Added styles.css and added signup form

--- Disrtibuted_electronics_store/settings.py
+++ Disrtibuted_electronics_store/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Electronics_App'
+    'Electronics_App',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
