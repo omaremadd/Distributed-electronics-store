9a0e80bf6f2af23ecadbb10cc3d6e0a0d1b7aeb7
Mohanad Ahmed <mohanadbador.ma@gmail.com>
2024-05-02 21:23:22 +0300

    Added Log in and Sign up urls

--- Electronics_App/urls.py
+++ Electronics_App/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from Electronics_App import views
 from django.contrib.auth import views as auth_views
-#from Frontend.html import index
 
 
 urlpatterns = [
@@ -12,6 +11,7 @@ urlpatterns = [
     path('payments/<pk>/', views.PaymentDetail.as_view(), name='payment_detail'),
     path('orders/', views.OrderList.as_view(), name='order_list'),
     path('orders/<pk>/', views.OrderDetail.as_view(), name='order_detail'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='index.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
