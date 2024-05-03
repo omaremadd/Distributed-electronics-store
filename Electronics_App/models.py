9f91f7c1ad0f4c9300c26271b96150cc52082f11
hebamaher <hebamaher25@gmail.com>
2024-05-03 21:46:18 +0300

    adding models

--- Electronics_App/models.py
+++ Electronics_App/models.py
@@ -6,7 +6,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-#from Electronics_App import admin           ########makes a circular import error
+from django.contrib import admin as django_admin
+#from Electronics_App import admin.py           ########makes a circular import error
 
 
 # Create your models here.
@@ -14,20 +15,22 @@ def validate_not_blank(value):
     if not value.strip():
         raise ValidationError("Phone number cannot be blank.")
 
-class admin(models.Model):
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=50,unique=True)
+class Seller(models.Model):
+    #admin = models.OneToOneField(django_admin, on_delete=models.CASCADE, default=None)
+    #name = models.CharField(max_length=50)
+    #password = models.CharField(max_length=50, unique=True)
+    #email = models.EmailField(max_length=50,unique=True)
     phone_regex = RegexValidator(regex=r'^\d+$', message="Phone number must contain only numbers.")
     phone = models.CharField(max_length=50, unique=True, validators=[phone_regex, validate_not_blank])
-    Admin_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    admin_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     department = models.CharField(max_length=50)
+    cash_balance = models.CharField(max_length=50)
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,default=None)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,default=None,primary_key=True)
     phone_regex = RegexValidator(regex=r'^\d+$', message="Phone number must contain only numbers.")
     phone = models.CharField(max_length=50, unique=True, validators=[phone_regex, validate_not_blank])
-    Customer_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    #Customer_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     
 
     # @receiver(post_save, sender=User)
@@ -40,18 +43,18 @@ class Customer(models.Model):
     #  instance.profile.save()
 
 class Payment(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     payment_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     method = models.CharField(max_length=50)
     amount = models.CharField(max_length=50)
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     Order_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     Delivery_date = models.DateField()
     Order_date = models.DateTimeField(auto_now_add=True)
     Complain = models.TextField()
-    address = models.TextField(default='',null=False)
+    address = models.TextField(default='', null=False)
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -65,11 +68,21 @@ class Product(models.Model):
     #Description 
     description = models.TextField(default='')
 
+class Cart(models.Model):
+    Cart_id = models.UUIDField(default=uuid.uuid4(), editable=False, primary_key=True, unique=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
+
+class Category(models.Model):
+    name = models.CharField(default='', primary_key=True, unique=True, max_length=200) #primary key
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 class update(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE);
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE);
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, default=None, on_delete=models.CASCADE)
+    sold_quantity = models.IntegerField(default=1)
+    remaining_quantity = models.IntegerField(default=1)
 
 
 class contain (models.Model):
