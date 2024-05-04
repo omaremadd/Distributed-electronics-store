22f0a23932677e83317194894480a300e0c5da63
tasnim <Tasnimsalahh@gmail.com>
2024-05-05 01:01:11 +0300

    fixed navbar bug

--- Electronics_App/static/js/navbar.js
+++ Electronics_App/static/js/navbar.js
@@ -1,25 +1,22 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
     var categories = [];
-    var categoryDropDown =this.getElementById('cat-dropdown');
-    async function getCategories(){
+    var categoryDropDown = document.getElementById('cat-dropdown');
+
+    async function getCategories() {
         var response = await fetch('http://127.0.0.1:8000/API/categories/?format=json');
         var finalresponse = await response.json();
         categories = finalresponse;
         console.log(categories);
         addCategories();
     }
-        getCategories();
-       
-    function addCategories(){
+
+    getCategories();
+
+    function addCategories() {
         var categHolder = ``;
-        for(var i=0;i<categories.length;i++){
-            categHolder+=`<li id="${categories[i].id}"><a class="dropdown-item" href="category/${categories[i].id}">${categories[i].title}</a></li>`;
+        for (var i = 0; i < categories.length; i++) {
+            categHolder += `<li id="${categories[i].id}"><a class="dropdown-item" href="/category/${categories[i].id}">${categories[i].title}</a></li>`;
         }
-        categoryDropDown.innerHTML=categHolder;
+        categoryDropDown.innerHTML = categHolder;
     }
-        
-    
 });
-
-
-    
