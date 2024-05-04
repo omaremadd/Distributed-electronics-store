0c7ae5d493411b01522851f8c77fe584cade893a
Omar Emad <167013174+omaremadd@users.noreply.github.com>
2024-05-04 19:13:14 +0300

    added category page

--- Electronics_App/static/js/category.js
+++ Electronics_App/static/js/category.js
@@ -1,35 +1,52 @@
-var title  = sessionStorage.getItem('title');
-var products = [];
+var category_id = Number(window.location.href.split("/").pop());
+// var title  = sessionStorage.getItem('title');
+var category = {};
 var productHolder=document.getElementById('productHolder');
- document.getElementById('categoryTitle').innerHTML=title;
- async function getProducts(){
-    var response = await fetch(`https://jsonplaceholder.typicode.com/photos`);
-    var finalResponse = await response.json();
-    // console.log(finalResponse[0].image);
-    products = finalResponse;
-    displayProducts();
-    // attachEvListener();
+async function getProducts(){
+  var response = await fetch(`http://${window.location.host}/API/categories/${category_id}/?format=json`);
+  var finalResponse = await response.json();
+  // console.log(finalResponse[0].image);
+  category = finalResponse;
+  displayProducts();
+  // attachEvListener();
+  document.getElementById('categoryTitle').innerHTML=category.title;
+  document.getElementsByTagName('title')[0].innerHTML=category.title;
 }
 getProducts();
 function displayProducts(){
     var productCard =``;
-    for(var i=0;i<6;i++){
+    for(var i=0;i<category.products.length;i++){
+      img_name=category.products[i].picture.split('/').pop();
         productCard+=`<div class="col-6 col-sm-3 mx-3 mb-6 float-on-hover-card">
-        <a href="product.html" class="card-link link-underline link-underline-opacity-0">
+        <a href="../product/${category.products[i].id}" class="card-link link-underline link-underline-opacity-0">
           <div class="card text-start">
-            <img class="card-img-top" src="${products[i].url}" alt="${products[i].title}" />
+            <img class="card-img-top" src="http://${window.location.host}/static/img/${img_name}" alt="${category.products[i].description}" style="height:200px; width:100%;object-fit: contain;"/>
             <div class="card-body">
-              <h4 class="card-title">${products[i].title}</h4>
+              <h4 class="card-title">${category.products[i].name}</h4>
               <p class="card-text">
-                <span class="placeholder col-7"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-4"></span>
+                <!-- <span class="placeholder col-7"></span> -->
+                <span class="">${category.products[i].quantity} left in stock</span><br>
+                <span class="">${category.products[i].price} EGP</span>
               </p>
-              <button class="btn btn-primary col-5">Add to Cart</button>
+              <button class="btn btn-primary col-5 ${cart_disabled(category.products[i].quantity)}" style="width:120px;">${cart_text(category.products[i].quantity)}</button>
             </div>
           </div>
         </a>
         </div>`
     }
     productHolder.innerHTML=productCard;
+}
+
+function cart_disabled(stock) {
+  if (stock == 0) {
+      return "disabled";
+  }
+  return "";
+}
+
+function cart_text(stock) {
+  if (stock == 0) {
+      return "Out of Stock";
+  }
+  return "Add to Cart";
 }
