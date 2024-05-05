5c500cbdb0492f17a0a8c346ccfbe99caa47593a
tasnim <Tasnimsalahh@gmail.com>
2024-05-05 17:52:22 +0300

    seller profile css

--- Electronics_App/static/js/seller_profile.js
+++ Electronics_App/static/js/seller_profile.js
@@ -1,10 +1,8 @@
 var userTable = document.getElementById('userTable');
 var username = document.getElementById('username');
-var firstname = document.getElementById('firstname');
-var lastname = document.getElementById('lastname');
 var email = document.getElementById('email');
-var phone = document.getElementById('phone');
 var user = null;
+
 async function getuser() {
     var response = await fetch(`http://${window.location.host}/API/profile/?format=json`);
     var finalResponse = await response.json();
@@ -17,7 +15,4 @@ getuser();
 function displayUser() {
     username.innerHTML = user.username;
     email.innerHTML = user.email;
-    firstname.innerHTML = user.first_name;
-    lastname.innerHTML = user.last_name;
-    phone.innerHTML = `+20${user.phone}`;
 }
