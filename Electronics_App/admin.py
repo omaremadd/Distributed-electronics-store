849400bdde0115f25140ed886ccfa9152016d449
Ahmed Hamed <71777180+AhmedHamed00@users.noreply.github.com>
2024-05-06 16:47:21 +0300

    registerd categories to admin

--- Electronics_App/admin.py
+++ Electronics_App/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Product, Payment, Order,Customer
+from .models import Product, Payment, Order,Customer ,Category
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(Customer)
-
+admin.site.register(Category)
